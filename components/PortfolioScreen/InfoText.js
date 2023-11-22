8ccb620c77cc24144502d62b1222868951721b58
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2023-11-22 21:49:54 +0530

    Minor Modifications in files...

--- components/PortfolioScreen/InfoText.js
+++ components/PortfolioScreen/InfoText.js
@@ -2,10 +2,10 @@ import {View,Text,StyleSheet} from 'react-native';
 import { COLORS } from "../constants";
 function InfoText(){
     return(
-        <View style={ styles.container }>
-              <Text style={ styles.textHeader}>Things to keep in mind!</Text>
-              <Text style={ styles.textStyle}>All the values are indicative. All your invetments are virtual...Investments profit and losses teaches you about the market. Try to make more profits from your investments that will allow you to push your rank... </Text>
-              <Text style={ styles.textBottom}>--CoinStack</Text>
+        <View style={styles.container}>
+              <Text style={styles.textHeader}>Things to keep in mind!</Text>
+              <Text style={styles.textStyle}>All the values are indicative. All your invetments are virtual...Investments profit and losses teaches you about the market. Try to make more profits from your investments that will allow you to push your rank... </Text>
+              <Text style={styles.textBottom}>--CoinStack</Text>
         </View>
     );
 }
