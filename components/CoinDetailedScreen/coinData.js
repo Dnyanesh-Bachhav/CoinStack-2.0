02c22553d86d6ee39a4f984dafb2c31eb779ebb6
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2023-12-12 01:35:04 +0530

    Text formatting in coin detail screen...

--- components/CoinDetailedScreen/coinData.js
+++ components/CoinDetailedScreen/coinData.js
@@ -1,18 +1,28 @@
 import react,{useState,useEffect} from 'react';
-import {View,Text,StyleSheet,ActivityIndicator,ScrollView} from 'react-native';
+import {View,Text,StyleSheet,ActivityIndicator,ScrollView, useWindowDimensions, Dimensions} from 'react-native';
 import { getCoinDataById } from '../../Services/requests';
 import Button from './Button';
 import { COLORS } from '../constants';
 import { Paragraph, SizableText } from 'tamagui';
+import RenderHTML from 'react-native-render-html';
 function CoinData({coinId,coinName }){
     const[loading,setLoading] = useState(true);
     const[coinData,setCoinData] = useState(null);
+    const { width } = useWindowDimensions();
+    const width1 = Dimensions.get("window").width;
+    const source = {
+        html: `
+      <p style='text-align:center;'>
+        Hello World!
+      </p>`
+      };
     const getCoinData = async ()=>{
         setLoading(true);
         console.log("CoinID in coindata: "+coinId);
         const data = await getCoinDataById(coinId);
         // console.log(data);
         setCoinData(data);
+
         // console.log("CoinID setCoinData: "+coinData);
         setLoading(false);
     }
@@ -26,12 +36,13 @@ function CoinData({coinId,coinName }){
             ? 
             <View style={styles.coinDataContainer}>
                 <SizableText size="$7" fontWeight="800" >{coinName}</SizableText>
-                <Paragraph size="$2" style={styles.descriptionText}>
-                    { !coinData ?
+                {/* <Paragraph size="$2"> */}
+                    <RenderHTML contentWidth={ 200 } source={{ html: !coinData ?
                         "Lorem ipsum dolor sit amet consectetur adipisicing elit. Quibusdam iusto doloremque dolores quis dolorum soluta id molestiae facere cumque ullam repellat ex, maiores perspiciatis? Reiciendis magnam quas possimus nemo at. Lorem ipsum dolor sit amet consectetur adipisicing elit. Qui obcaecati cum explicabo est, suscipit architecto nam earum. Recusandae expedita iure consectetur sed nihil possimus, nisi optio alias sunt vel ducimus!"
-                        : coinData.description.en
-                    }
-                </Paragraph>
+                        : `${coinData.description.en} `
+                    }}
+                    />
+                {/* </Paragraph> */}
                 {/* <Text style={styles.descriptionText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore itaque suscipit a, repellendus laboriosam quaerat magni, sequi nesciunt esse odit officia consectetur omnis doloremque expedita! Maxime sapiente doloremque ab eius? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo harum sed enim nihil, quo eius veniam distinctio, dolores sapiente quam laborum quibusdam saepe facere. Sed incidunt nisi nemo voluptatum quod?</Text>
                 <Text style={styles.descriptionText}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore itaque suscipit a, repellendus laboriosam quaerat magni, sequi nesciunt esse odit officia consectetur omnis doloremque expedita! Maxime sapiente doloremque ab eius? Lorem, ipsum dolor sit amet consectetur adipisicing elit. Explicabo harum sed enim nihil, quo eius veniam distinctio, dolores sapiente quam laborum quibusdam saepe facere. Sed incidunt nisi nemo voluptatum quod?</Text> */}
                 <View style={styles.btnGroup}>
