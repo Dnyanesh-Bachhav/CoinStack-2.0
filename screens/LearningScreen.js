cd42a82e48dc02bf4bad2d6947c28ed12de7a698
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2023-12-19 01:00:51 +0530

    Added sub section in learning screen...

--- screens/LearningScreen.js
+++ screens/LearningScreen.js
@@ -1,4 +1,4 @@
-import { StyleSheet, Text, View } from "react-native";
+import { Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 import { COLORS } from "../components/constants";
 import { H3, Paragraph, SizableText, Square } from "tamagui";
 import { ResizeMode, Video } from "expo-av";
@@ -7,11 +7,15 @@ import { Accordion } from "tamagui";
 import { ChevronDown } from "@tamagui/lucide-icons";
 import { Progress } from "tamagui";
 
+const WIDTH = Dimensions.get("window").width;
+const HEIGHT = WIDTH/1.9;
+
 function LearningScreen() {
   const video = useRef();
   return (
     <View style={styles.container}>
       <H3>Learn Crypto Trading</H3>
+      <ScrollView style={{ marginBottom: 10 }} showsVerticalScrollIndicator={ false }>
       {/* Video Section */}
       <Video
         ref={video}
@@ -38,34 +42,79 @@ function LearningScreen() {
       <SizableText fontFamily={"$body"} size={"$6"} fontWeight={800} style={{ marginTop: 10 }} >
         Course Content
       </SizableText>
-      <Accordion overflow="hidden" type="multiple" style={{ width: "100%", borderRadius: 10 }}>
-        <Accordion.Item value="a1">
-          <Accordion.Trigger flexDirection="row" justifyContent="space-between">
-            {({ open }) => (
-              <View style={{ width: "100%", elevation: 10 }} >
-                <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: "100%" }} >
-                    <Paragraph size="$5" >1. Introduction</Paragraph>
-                    <View style={{ backgroundColor: COLORS.lightGreen, borderRadius: 50, justifyContent: 'center', alignItems: 'center', padding: 4, borderWidth: 1, borderColor: COLORS.gray }} >
-                        <Square animation="quick" rotate={open ? "180deg" : "0deg"}>
-                            <ChevronDown size="$1" />
-                        </Square>
-                    </View>
+        <Accordion overflow="hidden" type="multiple" style={{ width: "100%", borderRadius: 10 }}>
+          <Accordion.Item value="a1">
+            <Accordion.Trigger flexDirection="row" justifyContent="space-between">
+              {({ open }) => (
+                <View style={{ width: "100%", elevation: 10 }}>
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: "100%" }} >
+                      <Paragraph size="$5" >1. Introduction</Paragraph>
+                      <View style={{ backgroundColor: COLORS.lightGreen, borderRadius: 50, justifyContent: 'center', alignItems: 'center', padding: 4, borderWidth: 1, borderColor: COLORS.gray }} >
+                          <Square animation="quick" rotate={open ? "180deg" : "0deg"}>
+                              <ChevronDown size="$1" />
+                          </Square>
+                      </View>
+                  </View>
+                  <Progress size={"$2"} value={60} style={{ width: "100%", backgroundColor: COLORS.lightGreen, marginTop: 12 }} >
+                    <Progress.Indicator animation="bouncy" style={{ color: COLORS.success, backgroundColor: "#51E445" }}  />
+                  </Progress>
                 </View>
-                <Progress size={"$2"} value={60} style={{ width: "100%", backgroundColor: COLORS.lightGreen, marginTop: 12 }} >
-                  <Progress.Indicator animation="bouncy" style={{ color: COLORS.success, backgroundColor: "#51E445" }}  />
-                </Progress>
-              </View>
-            )}
-          </Accordion.Trigger>
-          <Accordion.Content>
-            <Paragraph>
-              Cold showers can help reduce inflammation, relieve pain, improve
-              circulation, lower stress levels, and reduce muscle soreness and
-              fatigue.
-            </Paragraph>
-          </Accordion.Content>
-        </Accordion.Item>
-      </Accordion>
+              )}
+            </Accordion.Trigger>
+            <Accordion.Content style={{ marginTop: 10, width: '90%', alignSelf: 'flex-end' }} >
+              <Paragraph>
+              12Cold showers can help reduce inflammation
+                fatigue.
+              </Paragraph>
+            </Accordion.Content>
+            <Accordion.Content style={{ marginTop: 10, width: '90%', alignSelf: 'flex-end' }} >
+              <Paragraph>
+              12Cold showers can help reduce inflammation
+                fatigue.
+              </Paragraph>
+            </Accordion.Content>
+            <Accordion.Content style={{ marginTop: 10, width: '90%', alignSelf: 'flex-end' }} >
+              <Paragraph>
+              12Cold showers can help reduce inflammation
+                fatigue.
+              </Paragraph>
+            </Accordion.Content>
+            <Accordion.Content style={{ marginTop: 10, width: '90%', alignSelf: 'flex-end' }} >
+              <Paragraph>
+              12Cold showers can help reduce inflammation
+                fatigue.
+              </Paragraph>
+            </Accordion.Content>
+          </Accordion.Item>
+
+          <Accordion.Item value="a2" style={{ marginTop: 10 }} >
+            <Accordion.Trigger flexDirection="row" justifyContent="space-between">
+              {({ open }) => (
+                <View style={{ width: "100%", elevation: 10 }}>
+                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: "100%" }} >
+                      <Paragraph size="$5" >2. Introduction</Paragraph>
+                      <View style={{ backgroundColor: COLORS.lightGreen, borderRadius: 50, justifyContent: 'center', alignItems: 'center', padding: 4, borderWidth: 1, borderColor: COLORS.gray }} >
+                          <Square animation="quick" rotate={open ? "180deg" : "0deg"}>
+                              <ChevronDown size="$1" />
+                          </Square>
+                      </View>
+                  </View>
+                  <Progress size={"$2"} value={60} style={{ width: "100%", backgroundColor: COLORS.lightGreen, marginTop: 12 }} >
+                    <Progress.Indicator animation="bouncy" style={{ color: COLORS.success, backgroundColor: "#51E445" }}  />
+                  </Progress>
+                </View>
+              )}
+            </Accordion.Trigger>
+            <Accordion.Content style={{ marginTop: 10, width: '90%', alignSelf: 'flex-end' }} >
+              <Paragraph>
+              Cold showers can help reduce inflammation
+              
+              </Paragraph>
+            </Accordion.Content>
+          </Accordion.Item>
+        </Accordion>
+      </ScrollView>
+
     </View>
   );
 }
@@ -74,11 +123,12 @@ const styles = StyleSheet.create({
     flex: 1,
     paddingVertical: 10,
     paddingHorizontal: 10,
+    // paddingBottom: 40
     // backgroundColor: COLORS.white,
   },
   videoStyle: {
-    width: "90%",
-    height: "30%",
+    width: WIDTH ,
+    height: HEIGHT,
     alignSelf: "center",
     aspectRatio: "16/9",
     marginTop: 5,
