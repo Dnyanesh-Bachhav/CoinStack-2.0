2d1e56fd0ca4365e99e819715cb0496fd0ab7239
Dnyanesh Bachhav <dnyaneshbachhav2002@gmail.com>
2024-01-03 02:14:43 +0530

    Curriculum dropdown list dynamic implementation through firebase performed...

--- screens/LearningScreen.js
+++ screens/LearningScreen.js
@@ -1,4 +1,4 @@
-import { Dimensions, ScrollView, StyleSheet, View } from "react-native";
+import { ActivityIndicator, Dimensions, ScrollView, StyleSheet, Text, View } from "react-native";
 import { COLORS } from "../components/constants";
 import { H3, Paragraph, SizableText, Square } from "tamagui";
 import { ResizeMode, Video } from "expo-av";
@@ -8,7 +8,7 @@ import { ChevronDown } from "@tamagui/lucide-icons";
 import { Progress } from "tamagui";
 import Header from "../components/LearningScreen/Header";
 import app from "../firebaseConfig";
-import { collection, getDocs, getFirestore } from "firebase/firestore";
+import { collection, doc, getDoc, getDocs, getFirestore } from "firebase/firestore";
 
 const WIDTH = Dimensions.get("window").width;
 const HEIGHT = WIDTH/1.9;
@@ -16,14 +16,28 @@ const HEIGHT = WIDTH/1.9;
 function LearningScreen() {
   const video = useRef();
   const [ loading, setLoading ] = useState(false);
+  var currentKey = [];
+  var currentSubHeadings = [];
+  const [ courseDescription, setCourseDescription ] = useState(null);
+  const [ curriculum, setCurriculum ] = useState(null);
+  var key = "", value = [];
+
   async function getFirestoreData(){
     setLoading(true);
     const db = getFirestore(app);
-    const querySnapshot = await getDocs(collection(db, "testing"));
-querySnapshot.forEach((doc) => {
-  console.log(`${doc.id} => ${JSON.stringify(doc.data()) }`);
+    // Get Access to course collection
+    const collection_ref = collection(db, "testing");
+    // Get course description data i.e. needed on landing screen
+    const course_description_data = await getDoc(doc(collection_ref,"description"));
+    console.log(JSON.stringify(course_description_data.data()));
+    setCourseDescription(course_description_data.data());
+    // Get Curriculum list
+    const curriculum = await getDoc(doc(collection_ref,"curriculum"));
+    console.log(JSON.stringify(curriculum.data()));
+    setCurriculum(curriculum.data().data);
+    
+
   setLoading(false);
-});
   }
   useEffect(()=>{
     console.log("In a Learning Screen...");
@@ -33,80 +47,126 @@ querySnapshot.forEach((doc) => {
     <View style={styles.container}>
       {/* <H3>Learn Crypto Trading</H3> */}
       <Header />
-      <View style={styles.contentContainer}>
-      <ScrollView style={{ marginBottom: 10 }} showsVerticalScrollIndicator={ false }>
-      {/* Video Section */}
-      <Video
-        ref={video}
-        style={styles.videoStyle}
-        source={{
-          uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
-        }}
-        isLooping={true}
-        useNativeControls
-        resizeMode={ResizeMode.CONTAIN}
-      />
-      {/* ABOUT SECTION */}
-      <SizableText fontFamily={"$body"} size={"$6"} fontWeight={800} style={{ marginTop: 10 }} >
-        About
-      </SizableText>
-      <Paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hello ad nam
-        qui, alias iste, cum expedita cupiditate quibusdam et perspiciatis
-        placeat.orem ipsum dolor sit amet consectetur adipisicing elit. Impedit
-        quae omnis quibusdam voluptatibus veritatis iusto nobis sint dolores
-        culpa laboriosam{" "}
-      </Paragraph>
-      {/* COURSE CONTENT */}
-      <SizableText fontFamily={"$body"} size={"$6"} fontWeight={800} style={{ marginTop: 10 }} >
-        Course Content
-      </SizableText>
-        <Accordion overflow="hidden" type="multiple" style={{ width: "100%", borderRadius: 10 }}>
-          <Accordion.Item value="a1">
-            <Accordion.Trigger flexDirection="row" justifyContent="space-between">
-              {({ open }) => (
-                <View style={{ width: "100%", elevation: 10 }}>
-                  <View style={{ flexDirection: 'row', justifyContent: 'space-between', width: "100%" }} >
-                      <Paragraph size="$5" >1. Introduction</Paragraph>
-                      <View style={{ backgroundColor: COLORS.primaryFaint, borderRadius: 50, justifyContent: 'center', alignItems: 'center', padding: 4, borderWidth: 1, borderColor: COLORS.gray }} >
-                          <Square animation="quick" rotate={open ? "180deg" : "0deg"}>
+      {!loading ? (
+        <View style={styles.contentContainer}>
+          <ScrollView
+            style={{ marginBottom: 20 }}
+            showsVerticalScrollIndicator={false}
+          >
+            {/* Video Section */}
+            <Video
+              ref={video}
+              style={styles.videoStyle}
+              source={{
+                uri: "https://d23dyxeqlo5psv.cloudfront.net/big_buck_bunny.mp4",
+              }}
+              isLooping={true}
+              useNativeControls
+              resizeMode={ResizeMode.CONTAIN}
+            />
+            {/* ABOUT SECTION */}
+            <SizableText
+              fontFamily={"$body"}
+              size={"$6"}
+              fontWeight={800}
+              style={{ marginTop: 10 }}
+            >
+              About
+            </SizableText>
+            <Paragraph>
+              Lorem ipsum dolor sit amet, consectetur adipisicing elit. Hello ad
+              nam qui, alias iste, cum expedita cupiditate quibusdam et
+              perspiciatis placeat.orem ipsum dolor sit amet consectetur
+              adipisicing elit. Impedit quae omnis quibusdam voluptatibus
+              veritatis iusto nobis sint dolores culpa laboriosam{" "}
+            </Paragraph>
+            {/* COURSE CONTENT */}
+            <SizableText
+              fontFamily={"$body"}
+              size={"$6"}
+              fontWeight={800}
+              style={{ marginTop: 10 }}
+            >
+              Course Content
+            </SizableText>
+            <Accordion
+              overflow="hidden"
+              type="multiple"
+              style={{ width: "100%", borderRadius: 10, paddingBottom: 40 }}
+            >
+              {curriculum?.map((item, index) => (
+                <Accordion.Item value={`"a"${index + 1}`} style={{ marginTop: 5, elevation: 5 }} >
+                  {console.log(Object.entries(item))}
+                  <Accordion.Trigger
+                    flexDirection="row"
+                    justifyContent="space-between"
+                  >
+                    {({ open }) => (
+                      <View style={{ width: "100%", elevation: 5 }}>
+                        <View
+                          style={{
+                            flexDirection: "row",
+                            justifyContent: "space-between",
+                            width: "100%",
+                          }}
+                        >
+                          <Paragraph size={"$5"} fontWeight={"bold"} >{Object.entries(item)[0][0]}</Paragraph>
+                          <View
+                            style={{
+                              backgroundColor: COLORS.primaryFaint,
+                              borderRadius: 50,
+                              justifyContent: "center",
+                              alignItems: "center",
+                              padding: 4,
+                              borderWidth: 1,
+                              borderColor: COLORS.gray,
+                            }}
+                          >
+                            <Square
+                              animation="quick"
+                              rotate={open ? "180deg" : "0deg"}
+                            >
                               <ChevronDown size="$1" />
-                          </Square>
+                            </Square>
+                          </View>
+                        </View>
+                        <Progress
+                          size={"$2"}
+                          value={60}
+                          style={{
+                            width: "100%",
+                            backgroundColor: COLORS.lightGreen,
+                            marginTop: 12,
+                          }}
+                        >
+                          <Progress.Indicator
+                            animation="bouncy"
+                            style={{
+                              color: COLORS.success,
+                              backgroundColor: "#51E445",
+                            }}
+                          />
+                        </Progress>
                       </View>
-                  </View>
-                  <Progress size={"$2"} value={60} style={{ width: "100%", backgroundColor: COLORS.lightGreen, marginTop: 12 }} >
-                    <Progress.Indicator animation="bouncy" style={{ color: COLORS.success, backgroundColor: "#51E445" }}  />
-                  </Progress>
-                </View>
-              )}
-            </Accordion.Trigger>
-            <Accordion.Content style={{ marginTop: 10, width: '90%', alignSelf: 'flex-end' }} >
-              <Paragraph>
-              12Cold showers can help reduce inflammation
-                fatigue.
-              </Paragraph>
-            </Accordion.Content>
-            <Accordion.Content style={{ marginTop: 10, width: '90%', alignSelf: 'flex-end' }} >
-              <Paragraph>
-              12Cold showers can help reduce inflammation
-                fatigue.
-              </Paragraph>
-            </Accordion.Content>
-            <Accordion.Content style={{ marginTop: 10, width: '90%', alignSelf: 'flex-end' }} >
-              <Paragraph>
-              12Cold showers can help reduce inflammation
-                fatigue.
-              </Paragraph>
-            </Accordion.Content>
-            <Accordion.Content style={{ marginTop: 10, width: '90%', alignSelf: 'flex-end' }} >
-              <Paragraph>
-              12Cold showers can help reduce inflammation
-                fatigue.
-              </Paragraph>
-            </Accordion.Content>
-          </Accordion.Item>
+                    )}
+                  </Accordion.Trigger>
+                  {Object.entries(item)[0][1].map((subHeading, index1) => (
+                    <Accordion.Content
+                      style={{
+                        marginTop: 10,
+                        width: "90%",
+                        alignSelf: "flex-end",
+                        elevation: 2,
+                        borderRadius: 5
+                      }}
+                    >
+                      <Paragraph>{ subHeading }</Paragraph>
+                    </Accordion.Content>
+                  ))}
+                </Accordion.Item>
+              ))}
 
-          <Accordion.Item value="a2" style={{ marginTop: 10 }} >
+              {/* <Accordion.Item value="a2" style={{ marginTop: 10 }} >
             <Accordion.Trigger flexDirection="row" justifyContent="space-between">
               {({ open }) => (
                 <View style={{ width: "100%", elevation: 10 }}>
@@ -130,10 +190,13 @@ querySnapshot.forEach((doc) => {
               
               </Paragraph>
             </Accordion.Content>
-          </Accordion.Item>
-        </Accordion>
-      </ScrollView>
-    </View>
+          </Accordion.Item> */}
+            </Accordion>
+          </ScrollView>
+        </View>
+      ) : (
+        <ActivityIndicator size={"small"} />
+      )}
     </View>
   );
 }
